6e89cffffafdda7bd9bca9c2477e1ec75dd1ab01
Erkin303m <maxmudjonoverkin189@gmail.com>
2023-02-20 11:37:28 +0500

    .

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -19,24 +19,38 @@ import {
 } from '@mui/material';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { AiFillCar } from 'react-icons/ai';
+import { AiFillCar, AiOutlinePlusCircle } from 'react-icons/ai';
 import { BsFillPersonFill, BsPeopleFill } from 'react-icons/bs';
+
 import { w3cwebsocket as Socket } from 'websocket';
 import Swal from 'sweetalert2';
 import { useTranslation } from 'react-i18next';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 import Scrollbar from '../components/scrollbar';
 import { UserListHead } from '../sections/@dashboard/user';
 import USERLIST from '../_mock/user';
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 700,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 export default function UserPage() {
   const { t } = useTranslation();
   const TABLE_HEAD = [
     { id: 'id', label: t('ID'), alignRight: false },
-    { id: 'name', label: t('Name'), alignRight: false },
-    { id: 'lastName', label: t('Last name'), alignRight: false },
+    // { id: 'name', label: t('Name'), alignRight: false },
+    // { id: 'lastName', label: t('Last name'), alignRight: false },
     { id: 'number', label: t('Number'), alignRight: false },
-    { id: 'user', label: t('User'), alignRight: false },
+    // { id: 'user', label: t('User'), alignRight: false },
     { id: 'status', label: t('Chat'), alignRight: false },
   ];
 
@@ -104,9 +118,9 @@ export default function UserPage() {
     };
 
     await axios
-      .get(`http://185.217.131.179:8888/api/v1/company/staff-create/?driver=driver`, config)
+      .get(`http://185.217.131.179:8888/chat/rooms/get_user_for_room/`, config)
       .then((ress) => {
-        setMainData(get(ress, 'data.results', ''));
+        setMainData(get(ress, 'data', []));
       })
       .catch((err) => {
         console.log('error zayavka', err);
@@ -121,9 +135,10 @@ export default function UserPage() {
     };
 
     await axios
-      .get(`http://185.217.131.179:8888/api/v1/company/staff-create/?order_owner=order_owner`, config)
+      .get(`http://185.217.131.179:8888/chat/rooms/get_user_for_room/`, config)
       .then((ress) => {
-        setMainData2(get(ress, 'data.results', ''));
+        setMainData2(get(ress, 'data', []));
+        console.log('order owner', ress.data);
       })
       .catch((err) => {
         console.log('error zayavka', err);
@@ -167,6 +182,12 @@ export default function UserPage() {
     );
   };
 
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Helmet>
@@ -174,160 +195,12 @@ export default function UserPage() {
       </Helmet>
 
       <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-            {t('Chat')}
-          </Typography>
-          <BottomNavigation
-            showLabels
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-          >
-            <BottomNavigationAction label={t('Drivers')} icon={<AiFillCar />} />
-            <BottomNavigationAction label={t('Order owners')} icon={<BsFillPersonFill />} />
-            {/* <BottomNavigationAction label="Rooms" icon={<BsPeopleFill />} /> */}
-          </BottomNavigation>
-        </Stack>
-
-        {value === 0 ? (
-          <Card className="driverCard">
-            <Scrollbar>
-              <TableContainer sx={{ minWidth: 800 }}>
-                <Table>
-                  <UserListHead
-                    order={order}
-                    headLabel={TABLE_HEAD}
-                    rowCount={USERLIST.length}
-                    numSelected={selected.length}
-                    onRequestSort={handleRequestSort}
-                    onSelectAllClick={handleSelectAllClick}
-                  />
-                  <TableBody>
-                    {mainData.map((row, i) => {
-                      return (
-                        <TableRow hover key={i} tabIndex={-1}>
-                          <TableCell component="th" scope="row" padding="none">
-                            <Stack direction="row" alignItems="center" spacing={2}>
-                              <Typography variant="subtitle2" className="zayavkaName">
-                                {get(row, 'id', '')}
-                              </Typography>
-                            </Stack>
-                          </TableCell>
-
-                          <TableCell align="left"> {get(row, 'first_name', '')}</TableCell>
-
-                          <TableCell align="left"> {get(row, 'last_name', '')}</TableCell>
-                          <TableCell align="left"> {get(row, 'phone_number', '')}</TableCell>
-
-                          <TableCell align="left"> {get(row, 'user', '')}</TableCell>
-
-                          <TableCell align="left">
-                            <Button onClick={() => createRoom(row.user)}>{t('Create Room')}</Button>
-                          </TableCell>
-                        </TableRow>
-                      );
-                    })}
-                    {emptyRows > 0 && (
-                      <TableRow style={{ height: 53 * emptyRows }}>
-                        <TableCell colSpan={6} />
-                      </TableRow>
-                    )}
-                  </TableBody>
-
-                  {mainData.length <= 0 && (
-                    <TableBody>
-                      <TableRow>
-                        <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                          <Paper
-                            sx={{
-                              textAlign: 'center',
-                            }}
-                          >
-                            <Typography variant="h6" paragraph>
-                              {t('Not found')}
-                            </Typography>
-                          </Paper>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  )}
-                </Table>
-              </TableContainer>
-            </Scrollbar>
-          </Card>
-        ) : null}
-
-        {value === 1 ? (
-          <Card>
-            <Scrollbar>
-              <TableContainer sx={{ minWidth: 800 }}>
-                <Table>
-                  <UserListHead
-                    order={order}
-                    headLabel={TABLE_HEAD}
-                    rowCount={USERLIST.length}
-                    numSelected={selected.length}
-                    onRequestSort={handleRequestSort}
-                    onSelectAllClick={handleSelectAllClick}
-                  />
-                  <TableBody>
-                    {mainData2.map((row, i) => {
-                      return (
-                        <TableRow hover key={i} tabIndex={-1}>
-                          <TableCell component="th" scope="row" padding="none">
-                            <Stack direction="row" alignItems="center" spacing={2}>
-                              <Typography variant="subtitle2" className="zayavkaName">
-                                {row.id}
-                              </Typography>
-                            </Stack>
-                          </TableCell>
-
-                          <TableCell align="left">{row.first_name}</TableCell>
-
-                          <TableCell align="left">{row.last_name}</TableCell>
-                          <TableCell align="left">{row.phone_number}</TableCell>
-
-                          <TableCell align="left">{row.user}</TableCell>
-
-                          <TableCell align="left">
-                            <Button onClick={() => createRoom(row.user)}>{t('Create Room')}</Button>
-                          </TableCell>
-                        </TableRow>
-                      );
-                    })}
-                    {emptyRows > 0 && (
-                      <TableRow style={{ height: 53 * emptyRows }}>
-                        <TableCell colSpan={6} />
-                      </TableRow>
-                    )}
-                  </TableBody>
-
-                  {mainData2.length <= 0 && (
-                    <TableBody>
-                      <TableRow>
-                        <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                          <Paper
-                            sx={{
-                              textAlign: 'center',
-                            }}
-                          >
-                            <Typography variant="h6" paragraph>
-                              {t('Not found')}
-                            </Typography>
-                          </Paper>
-                        </TableCell>
-                      </TableRow>
-                    </TableBody>
-                  )}
-                </Table>
-              </TableContainer>
-            </Scrollbar>
-          </Card>
-        ) : null}
-
-        <h1>{t('People who have written before')}</h1>
+        <div className='plusCard'>
+          <h1>{t('People who have written before')}</h1>
+          <Button className='btnPlus' onClick={() => handleOpen()}>
+            <AiOutlinePlusCircle />
+          </Button>
+        </div>
 
         <Card>
           {/* <input type="text" placeholder="Search Rooms" className="input3" onChange={(v) => search(v.target.value)} /> */}
@@ -390,6 +263,181 @@ export default function UserPage() {
           </Scrollbar>
         </Card>
       </Container>
+
+      {/* 2-modal */}
+
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style} className="bigModalCard">
+            <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+              <Typography variant="h4" gutterBottom>
+                {t('Chat')}
+              </Typography>
+              <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              >
+                <BottomNavigationAction label={t('Drivers')} icon={<AiFillCar />} />
+                <BottomNavigationAction label={t('Order owners')} icon={<BsFillPersonFill />} />
+                {/* <BottomNavigationAction label="Rooms" icon={<BsPeopleFill />} /> */}
+              </BottomNavigation>
+            </Stack>
+            {value === 1 ? (
+              <Card>
+                <Scrollbar>
+                  <TableContainer sx={{ minWidth: 500 }}>
+                    <Table>
+                      <UserListHead
+                        order={order}
+                        headLabel={TABLE_HEAD}
+                        rowCount={USERLIST.length}
+                        numSelected={selected.length}
+                        onRequestSort={handleRequestSort}
+                        onSelectAllClick={handleSelectAllClick}
+                      />
+                      <TableBody>
+                        {mainData2.map((row, i) => {
+                          return (
+                            <TableRow
+                              hover
+                              key={i}
+                              tabIndex={-1}
+                              className={get(row, 'role', '') === 'order_owner' ? '' : 'none'}
+                            >
+                              <TableCell component="th" scope="row" padding="none">
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                  <Typography variant="subtitle2" className="zayavkaName">
+                                    {row.id}
+                                  </Typography>
+                                </Stack>
+                              </TableCell>
+
+                              {/* <TableCell align="left">{row.first_name}</TableCell>
+
+                          <TableCell align="left">{row.last_name}</TableCell> */}
+                              <TableCell align="left">{row.phone_number}</TableCell>
+
+                              {/* <TableCell align="left">{row.user}</TableCell> */}
+
+                              <TableCell align="left">
+                                <Button onClick={() => createRoom(row.user)}>{t('Create Room')}</Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                        {emptyRows > 0 && (
+                          <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                          </TableRow>
+                        )}
+                      </TableBody>
+
+                      {mainData2.length <= 0 && (
+                        <TableBody>
+                          <TableRow>
+                            <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                              <Paper
+                                sx={{
+                                  textAlign: 'center',
+                                }}
+                              >
+                                <Typography variant="h6" paragraph>
+                                  {t('Not found')}
+                                </Typography>
+                              </Paper>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      )}
+                    </Table>
+                  </TableContainer>
+                </Scrollbar>
+              </Card>
+            ) : null}
+            {value === 0 ? (
+              <Card className="driverCard">
+                <Scrollbar>
+                  <TableContainer sx={{ minWidth: 500 }}>
+                    <Table>
+                      <UserListHead
+                        order={order}
+                        headLabel={TABLE_HEAD}
+                        rowCount={USERLIST.length}
+                        numSelected={selected.length}
+                        onRequestSort={handleRequestSort}
+                        onSelectAllClick={handleSelectAllClick}
+                      />
+                      <TableBody>
+                        {mainData.map((row, i) => {
+                          return (
+                            <TableRow
+                              hover
+                              key={i}
+                              tabIndex={-1}
+                              className={get(row, 'role', '') === 'driver' ? '' : 'none'}
+                            >
+                              <TableCell component="th" scope="row" padding="none">
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                  <Typography variant="subtitle2" className="zayavkaName">
+                                    {get(row, 'id', '')}
+                                  </Typography>
+                                </Stack>
+                              </TableCell>
+                              {/* 
+                          <TableCell align="left"> {get(row, 'first_name', '')}</TableCell>
+
+                          <TableCell align="left"> {get(row, 'last_name', '')}</TableCell> */}
+                              <TableCell align="left"> {get(row, 'phone_number', '')}</TableCell>
+
+                              {/* <TableCell align="left"> {get(row, 'user', '')}</TableCell> */}
+
+                              <TableCell align="left">
+                                <Button onClick={() => createRoom(row.user)}>{t('Create Room')}</Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                        {emptyRows > 0 && (
+                          <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                          </TableRow>
+                        )}
+                      </TableBody>
+
+                      {mainData.length <= 0 && (
+                        <TableBody>
+                          <TableRow>
+                            <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                              <Paper
+                                sx={{
+                                  textAlign: 'center',
+                                }}
+                              >
+                                <Typography variant="h6" paragraph>
+                                  {t('Not found')}
+                                </Typography>
+                              </Paper>
+                            </TableCell>
+                          </TableRow>
+                        </TableBody>
+                      )}
+                    </Table>
+                  </TableContainer>
+                </Scrollbar>
+              </Card>
+            ) : null}
+            <p className="productNameTitle">New chat</p>
+          </Box>
+        </Modal>
+      </div>
     </>
   );
 }
